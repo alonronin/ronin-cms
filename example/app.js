c4f4ac69192ac5d6815b7500b1de01289dc0e5c6
Alon Valadji <alon@ronin.co.il>
2013-10-22 06:12:43 +0300

    cleanup example

--- example/app.js
+++ example/app.js
@@ -9,20 +9,7 @@ var app = express();
 ronin.express = express;
 ronin.mongoose = mongoose;
 
-app.get('/users', function(req, res){
-    ronin.cms.models.users.find().exec(function(err, result){
-        res.json(result);
-    })
-});
-
 app.use(ronin());
-
-var schema = new mongoose.Schema({
-    name: String,
-    email: String
-});
-
-ronin.cms.models.users = mongoose.model('users', schema);
 
 mongoose.connect('mongodb://localhost/ronin-cms-example');
 
